5837b31f4d45db1d92ef3c52311e8e603ba0ab5e
Chibariyooh <62876107+Chibariyooh@users.noreply.github.com>
2020-11-22 00:44:03 +0900

    user_skills controllreの修正

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,2 +1,18 @@
 class User_skillsController < ApplicationController
+    def create
+        current_user.user_skills.create!(skill_id: params[:skill_id])
+        @user_skill.save
+        redirect_to root_path
+    end
+
+    def update
+        @user_skill = @user.user_skills.find(parmas[:user_skill_id])
+        @user_skill.save
+        @user_skill.update(user_skill_params)
+    end
+
+    private
+    def user_skill_params
+        params.require(:user_skills).permit(:skill_level,:user_id,:skill_id)
+    end
 end
